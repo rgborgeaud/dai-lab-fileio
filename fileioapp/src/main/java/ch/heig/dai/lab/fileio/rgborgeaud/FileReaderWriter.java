de153e5f30abc796f1563138fb563a1439053098
rgborgeaud <gael.borgeaud@heig-vd.ch>
2024-10-14 16:23:43 +0200

    FileReaderWriter for rgborgeaud

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/rgborgeaud/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/rgborgeaud/FileReaderWriter.java
@@ -12,10 +12,28 @@ public class FileReaderWriter {
      * @return the content of the file as a String, or null if an error occurred.
      */
     public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
+
+        try(BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(file),
+                        encoding))) {
+
+            String str, text = "";
+
+            while((str = reader.readLine()) != null) {
+                text = text.concat(str);
+            }
+
+            return text.concat("\n");
+
+        } catch(IOException e) {
+            System.out.println("Exception : " + e);
+            return null;
+        }
+        // TODO: Implement the method body here.
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to close the streams and readers at the end.
-        return null;
+
     }
 
     /**
@@ -26,9 +44,22 @@ public class FileReaderWriter {
      * @return true if the file was written successfully, false otherwise
      */
     public boolean writeFile(File file, String content, Charset encoding) {
+
+        try (var writer = new BufferedWriter(
+                new OutputStreamWriter(
+                        new FileOutputStream(file),
+                        encoding
+                ))) {
+
+            writer.write(content);
+            return true;
+
+        } catch (IOException e) {
+            System.out.println("Exception : " + e);
+            return false;
+        }
         // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to flush the data and close the streams and readers at the end.
-        return false;
     }
 }
