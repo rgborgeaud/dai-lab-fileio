2a5f92f96583d6b5a380a923ff2003e988f8ec6d
rgborgeaud <160333137+rgborgeaud@users.noreply.github.com>
2024-10-14 16:26:15 +0200

    Update EncodingSelectorTest.java

--- fileioapp/src/test/java/ch/heig/dai/lab/fileio/rgborgeaud/EncodingSelectorTest.java
+++ fileioapp/src/test/java/ch/heig/dai/lab/fileio/rgborgeaud/EncodingSelectorTest.java
@@ -12,7 +12,7 @@ public class EncodingSelectorTest {
     private final EncodingSelector selector = new EncodingSelector();
 
     @Test
-    @Disabled
+    
     public void encodingTest() {
         assertEquals (StandardCharsets.UTF_8, selector.getEncoding(new File("file1.utf8")));
         assertEquals (StandardCharsets.US_ASCII, selector.getEncoding(new File("file1.txt")));
@@ -21,14 +21,14 @@ public class EncodingSelectorTest {
     }
 
     @Test
-    @Disabled
+    
     public void nullTest() {
         assertEquals (null, selector.getEncoding(new File("file1.utf")));
         assertEquals (null, selector.getEncoding(new File("file1")));
     }
 
     @Test
-    @Disabled
+    
     public void dotsTests() {
         assertEquals (StandardCharsets.UTF_8, selector.getEncoding(new File("file1.txt.utf8")));
         assertEquals (StandardCharsets.US_ASCII, selector.getEncoding(new File("file1.utf8.txt")));
